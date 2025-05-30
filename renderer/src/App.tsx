5c2c9c571d14a3cb46e578cdbadd5de46abdf956
Andres Antillon <aantillon.leiva@gmail.com>
2025-05-30 15:59:53 -0600

    init tauri app

---
+++ renderer/src/App.tsx
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="retro-container">
+      <div className="retro-screen">
+        <div className="scan-lines"></div>
+        <div className="content">
+          <h1 className="retro-title">
+            <span className="blink">●</span> HELLO WORLD <span className="blink">●</span>
+          </h1>
+          <div className="retro-border">
+            <p className="retro-text">
+              WELCOME TO THE FUTURE
+            </p>
+            <p className="retro-subtext">
+              [SYSTEM INITIALIZED]
+            </p>
+          </div>
+          <div className="retro-footer">
+            <span className="cursor-blink">█</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
